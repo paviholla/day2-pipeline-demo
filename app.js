ca9fe7bd1cfb6a2c060de1a9394d30483ad8973a
Pavithra Holla <pavithra.holla@incontact.com>
2018-09-26 14:01:20 -0600

    Initial CheckIn

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/marco', function(req, res){
+    res.status(200).send('Polo!');
+});
+
+var server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log('The Marco Polo app is listening on port %s!', port);
+});
+
+module.exports = server;
